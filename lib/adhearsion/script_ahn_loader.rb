239b3939a8220c2171e92fb82878c381468b7925
kares <self@kares.org>
2015-03-03 07:02:19 +0100

    re-using `in_ahn_application?` in load_script_ahn

--- lib/adhearsion/script_ahn_loader.rb
+++ lib/adhearsion/script_ahn_loader.rb
@@ -11,9 +11,8 @@ module Adhearsion
     def self.load_script_ahn(path = Dir.pwd)
       path = Pathname.new(path).expand_path
       until path.root?
-        script = File.join(path, SCRIPT_AHN)
-        if File.exists?(script)
-          load script
+        if in_ahn_application?(path.to_s)
+          load File.join(path, SCRIPT_AHN)
           return true
         end
         path = path.parent
@@ -23,7 +22,7 @@ module Adhearsion
 
     def self.in_ahn_application?(path = nil)
       return File.exists? SCRIPT_AHN unless path
-      Dir.chdir(path) { File.exists? SCRIPT_AHN }
+      Dir.chdir(path) { return File.exists? SCRIPT_AHN }
     end
 
     def self.in_ahn_application_subdirectory?(path = nil)
